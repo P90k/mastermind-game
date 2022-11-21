040c1844828cb94d3cd0d5d2a142197b448be37d
Andreas Jansson <andreas.jansson234@gmail.com>
2022-11-21 19:01:33 +0100

    Create class Gamesession & Mastermind
    class mastermind will hold the state of the game. Write constructor
    method. Constructor method will store state of game, such as
    code colors, key pegs and a secret code.
    Write method for generating secret code and a method that will
    print the secret code to the screen.

---
+++ mastermind.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class Mastermind
+  def initialize
+    @code_colors = %w[red blue yellow green black white]
+    @key_pegs = %w[x x x x]
+    @secret_code = build_secret_code
+  end
+
+  def build_secret_code
+    secret_code = []
+
+    until secret_code.length == 4
+      random_color = @code_colors[rand(4)]
+      secret_code.include?(random_color) ? next : secret_code.push(random_color)
+    end
+    secret_code
+  end
+
+  def secret_code
+    puts @secret_code
+  end
+end
+
+class GameSession < Mastermind
+end
+mastermind = Mastermind.new
+mastermind.secret_code
