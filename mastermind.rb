284c488a722276678002e8104842dd0f6c9552cb
Andreas Jansson <andreas.jansson234@gmail.com>
2022-12-02 12:07:17 +0100

    Define gameflow and game ending
    put together existing methods into a functional game, make sure that the
    game ends when the correct code is guessed.

--- mastermind.rb
+++ mastermind.rb
@@ -3,7 +3,7 @@
 class Mastermind
   def initialize
     @code_colors = %w[red blue yellow green black white]
-    @feedback = %w[x x x x]
+    @feedback = %w[- - - -]
     @secret_code = build_secret_code
   end
 
@@ -18,22 +18,29 @@ class Mastermind
   end
 
   def secret_code
-    puts @secret_code
+    p @secret_code
   end
 
   def player_guess
+    p 'Place your guess'
     gets.chomp.split(' ')
   end
 
   def feedback(user_guess)
     user_guess.each_with_index do |element, index|
-      if @secret_code.include?(element) && @secret_code.index(element) == index
-        @feedback[index] = 'black'
-      elsif @secret_code.include?(element)
-        @feedback[index] = 'white'
-      end
+      @feedback[index] = if @secret_code.include?(element) && @secret_code.index(element) == index
+                           'black'
+                         elsif @secret_code.include?(element)
+                           'white'
+                         else
+                           '-'
+                         end
     end
     @feedback
+  end
+
+  def determine_end(count)
+    count + 1 > 12
   end
 end
 
@@ -41,8 +48,24 @@ class GameSession < Mastermind
   def initialize
     @game_session = super
   end
+
+  def game_session
+    rounds = 0
+    p 'Welcome to MasterMind, can you crack the code?'
+    puts ''
+    until determine_end(rounds)
+      guess = player_guess
+      puts "Your guess: #{guess}"
+      puts ''
+      puts "Feedback on your guess: #{feedback(guess)}"
+      rounds += 1
+      if feedback(guess).count('black') == 4
+        p 'Congratulations, you cracked the code!'
+        break
+      end
+    end
+  end
 end
 
-mastermind = Mastermind.new
-p mastermind.secret_code
-p mastermind.feedback(%w[red green yellow blue])
+gamesession = GameSession.new
+gamesession.game_session
